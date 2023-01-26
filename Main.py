f3f2e0ba6b2f6e45a1c2b72983a38956d6a0dc4d
Rgamer43 <54045197+Rgamer43@users.noreply.github.com>
2023-01-26 16:38:07 -0500

    Stuff to read from images and a bunch of images

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ import cv2
 
 #Create the Apriltag Detector
 at_detector = Detector(
-   families="tag36h11 tag16h5",
+   families="tag16h5",
    nthreads=1,
    quad_decimate=1.0,
    quad_sigma=0.0,
@@ -16,85 +16,96 @@ at_detector = Detector(
    debug=0
 )
 
-#Init NetworkTables
-logging.basicConfig(level=logging.DEBUG)
-NetworkTables.initialize()
-sd = NetworkTables.getTable("SmartDashboard")
-sd.putNumberArray("pos", {-1, -1, -1})
-sd.putNumber("rot", -1)
+#Uncomment below 5 lines if reading from image file instead of camera
+img = cv2.imread('images\\IMG_0823.png', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('img', img)
 
-#create a video capture
-cap = cv2.VideoCapture(0)
+#Detect the apriltags
+response = at_detector.detect(img)
+print("\n")
+print(response)
+print("\nTags Detected: " + str(len(response)))
 
-cap.set(cv2.CAP_PROP_EXPOSURE, -6)
+# #Init NetworkTables
+# logging.basicConfig(level=logging.DEBUG)
+# NetworkTables.initialize()
+# sd = NetworkTables.getTable("SmartDashboard")
+# sd.putNumberArray("pos", {-1, -1, -1})
+# sd.putNumber("rot", -1)
 
-bounds = 10
+# # create a video capture
+# cap = cv2.VideoCapture(0)
 
-colorBlueUnsaturated = 125
+# cap.set(cv2.CAP_PROP_EXPOSURE, -6)
 
-while True:
-    #read frame from the camera
-    _, img = cap.read()
+# bounds = 10
 
-    #resize camera, used later with contours
-    img = cv2.resize(img, (800,500))
+# colorBlueUnsaturated = 125
 
-    #image without masks
-    initImg = img
+# while True:
+#     # read frame from the camera
+#     _, img = cap.read()
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#     #resize camera, used later with contours
+#     img = cv2.resize(img, (800,500))
 
-    #only show things between certain saturations
-    mask = cv2.inRange(img, (0, 0, 50), (360, 255, 255))
+#     #image without masks
+#     initImg = img
 
-    if keyboard.is_pressed('d'):
-        colorBlueUnsaturated += 1
-        print(colorBlueUnsaturated)
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    if keyboard.is_pressed('a'):
-        colorBlueUnsaturated -= 1
-        print(colorBlueUnsaturated)
+#     #only show things between certain saturations
+#     mask = cv2.inRange(img, (0, 0, 50), (360, 255, 255))
 
-    #only show things between color range
-    mask2 = cv2.inRange(img, (colorBlueUnsaturated - bounds, 0, 0), (colorBlueUnsaturated + bounds, 255, 255))
+#     if keyboard.is_pressed('d'):
+#         colorBlueUnsaturated += 1
+#         print(colorBlueUnsaturated)
 
-    #show both masks at once, only shows pixels picked up by both masks
-    mask3 = cv2.bitwise_and(mask, mask2)
+#     if keyboard.is_pressed('a'):
+#         colorBlueUnsaturated -= 1
+#         print(colorBlueUnsaturated)
 
-    #improve accuracy of the mask detection
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(3,3))
-    mask3 = cv2.erode(mask3, kernel)
-    mask3 = cv2.dilate(mask3, kernel)
+#     #only show things between color range
+#     mask2 = cv2.inRange(img, (colorBlueUnsaturated - bounds, 0, 0), (colorBlueUnsaturated + bounds, 255, 255))
 
-    #find countours, basically finding the edges of the game piece
-    cnts = cv2.findContours(mask3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     #show both masks at once, only shows pixels picked up by both masks
+#     mask3 = cv2.bitwise_and(mask, mask2)
 
-    for i in cnts[0]:
-        M = cv2.moments(i)
-        cX = int((M["m10"] / M["m00"])) / 800
-        cY = int((M["m01"] / M["m00"])) / 500
+#     #improve accuracy of the mask detection
+#     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(3,3))
+#     mask3 = cv2.erode(mask3, kernel)
+#     mask3 = cv2.dilate(mask3, kernel)
 
-        initImg = cv2.circle(initImg,(math.floor(cX * 800), math.floor(cY * 500)), 20, (0, 0, 255), -1)
-        img = cv2.circle(initImg,(math.floor(cX * 800), math.floor(cY * 500)), 20, (0, 0, 255), -1)
+#     #find countours, basically finding the edges of the game piece
+#     cnts = cv2.findContours(mask3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        break
+#     for i in cnts[0]:
+#         M = cv2.moments(i)
+#         cX = int((M["m10"] / M["m00"])) / 800
+#         cY = int((M["m01"] / M["m00"])) / 500
 
-    img = cv2.bitwise_and(img, img, mask=mask3)
+#         initImg = cv2.circle(initImg,(math.floor(cX * 800), math.floor(cY * 500)), 20, (0, 0, 255), -1)
+#         img = cv2.circle(initImg,(math.floor(cX * 800), math.floor(cY * 500)), 20, (0, 0, 255), -1)
 
-    #show the capture from camera
-    cv2.imshow("image with masks", img)
-    cv2.imshow("initial image", initImg)
+#         break
+
+#     img = cv2.bitwise_and(img, img, mask=mask3)
+
+#     #show the capture from camera
+#     cv2.imshow("image with masks", img)
+#     cv2.imshow("initial image", initImg)
+
+#     cv2.waitKey(0)
     
-    #Uncomment below two lines if reading from image file instead of camera
-    # img = cv2.imread('images\\IMG_0442.png', cv2.IMREAD_GRAYSCALE)
-    # cv2.imshow('img', img)
-
-    cv2.waitKey(0)
+#     #Detect the apriltags
+#     response = at_detector.detect(img)
+#     print("\n")
+#     print(response)
     
     #Detect the apriltags
-    response = at_detector.detect(img)
-    print(response)
+    # response = at_detector.detect(img)
+    # print(response)
 
     #wait in milliseconds
-    cv2.waitKey(5)
+    # cv2.waitKey(5)
     #100 lines
