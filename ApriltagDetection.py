8e24908e10b8ab21d6b8fa40a7be60b86ee281e3
Owen Beck <oebeck@gmail.com>
2023-03-15 16:44:38 -0400

    Added NetworkTables

---
+++ ApriltagDetection.py
@@ -0,0 +1,93 @@
+from RioComms import RioComms
+from pupil_apriltags import Detector
+import cv2
+import ast
+import re
+
+rioComms = RioComms("10.40.26.2")
+tableName = "apriltags"
+
+#Create the Apriltag Detector
+
+#  Working Parameters:
+#    quad_decimate=0.3
+#    quad_sigma=0.35
+#    refine_edges=1
+#    decode_sharpening=0.25
+
+at_detector = Detector(
+   families="tag16h5",
+   nthreads=1,
+   quad_decimate=0.3,
+   quad_sigma=0.35,
+   refine_edges=1,
+   decode_sharpening=0.25,
+   debug=0
+)
+
+#Tag size in meters
+tag_size = 0.1524
+
+#cap is 640 pixels by 480 pixels
+cap = cv2.VideoCapture(1)
+
+while True:
+  _, img = cap.read()
+
+  imgGrayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+  apriltags = at_detector.detect(imgGrayscale)
+
+  apriltags = [x for x in apriltags if x.hamming == 0]
+
+  if len(apriltags) > 0:
+    print("\nApriltags:")
+
+  for i in range(len(apriltags)):
+    centerXY = ast.literal_eval((re.sub(" +", " ", ((str(apriltags[i].center).replace("[", "")).replace("]", "")).strip())).replace(" ", ", "))
+    centerXY = list(centerXY)
+    centerXY[0] = centerXY[0] - 320
+    centerXY[1] = centerXY[1] - 240
+    print(" Tag:", apriltags[i].tag_id, "\n  X:", str(centerXY[0]), "\n  Y:", str(centerXY[1]))
+    if (apriltags[i].tag_id >= 1 and apriltags[i].tag_id <= 3) or apriltags[i].tag_id == 5:
+      img = cv2.putText(img, str(apriltags[i].tag_id), (int(centerXY[0]) + 300, int(centerXY[1]) + 260), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 6, 2)
+      img = cv2.putText(img, str(int(centerXY[0])) + ", " + str(int(centerXY[1])), (int(centerXY[0]) + 300, int(centerXY[1]) + 300), cv2.FONT_HERSHEY_SIMPLEX, 2/3, (0, 0, 255), 3, 2)
+    elif (apriltags[i].tag_id >= 6 and apriltags[i].tag_id <= 8) or apriltags[i].tag_id == 4:
+      img = cv2.putText(img, str(apriltags[i].tag_id), (int(centerXY[0]) + 300, int(centerXY[1]) + 260), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 6, 2)
+      img = cv2.putText(img, str(int(centerXY[0])) + ", " + str(int(centerXY[1])), (int(centerXY[0]) + 300, int(centerXY[1]) + 300), cv2.FONT_HERSHEY_SIMPLEX, 2/3, (255, 0, 0), 3, 2)
+
+    rioComms.send("apriltags", "Tag " + str(apriltags[i].tag_id) + " X", centerXY[0])
+    rioComms.send("apriltags", "Tag " + str(apriltags[i].tag_id) + " Y", centerXY[1])
+
+
+
+    print(apriltags[i].corners)
+
+    cornersXYValues = list(re.sub(" +", " ", ''.join((str(apriltags[i].corners).replace("[", "").replace("]", "").strip()).splitlines())).split(" "))
+
+    tagWidth = abs(float(cornersXYValues[4]) - float(cornersXYValues[6]))
+
+    rioComms.send("apriltags", "Tag" + str(apriltags[i].tag_id) + "Width", tagWidth)
+
+
+  rioComms.send("apriltags", "Tags", len(apriltags))
+
+  apriltagIDs = []
+  for i in range(len(apriltags)):
+    apriltagIDs.append(apriltags[i].tag_id)
+
+  for i in range(8):
+    if apriltagIDs.count(i+1) > 0:
+      tagVisible = 1
+    else:
+      tagVisible = 0
+
+    rioComms.send("apriltags", "Tag " + str(i + 1) + " Visible", tagVisible)
+
+  cv2.imshow("Camera", img)
+
+  cv2.waitKey(5)
+
+
+
+
--- Main.py
+++ Main.py
@@ -1,93 +1,71 @@
-from RioComms import RioComms
-from pupil_apriltags import Detector
 import cv2
-import ast
-import re
+from RioComms import RioComms
+import math
 
 rioComms = RioComms("10.40.26.2")
-tableName = "apriltags"
 
-#Create the Apriltag Detector
-
-#  Working Parameters:
-#    quad_decimate=0.3
-#    quad_sigma=0.35
-#    refine_edges=1
-#    decode_sharpening=0.25
-
-at_detector = Detector(
-   families="tag16h5",
-   nthreads=1,
-   quad_decimate=0.3,
-   quad_sigma=0.35,
-   refine_edges=1,
-   decode_sharpening=0.25,
-   debug=0
-)
-
-#Tag size in meters
-tag_size = 0.1524
-
-#cap is 640 pixels by 480 pixels
 cap = cv2.VideoCapture(1)
+
+cap.set(cv2.CAP_PROP_EXPOSURE, -6)
+
 
 while True:
   _, img = cap.read()
 
-  imgGrayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  img = cv2.resize(img, (800, 500))
 
-  apriltags = at_detector.detect(imgGrayscale)
+  initImg = img
 
-  apriltags = [x for x in apriltags if x.hamming == 0]
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-  if len(apriltags) > 0:
-    print("\nApriltags:")
+  mask = cv2.inRange(img, (0, 0, 0), (25, 255, 255))
 
-  for i in range(len(apriltags)):
-    centerXY = ast.literal_eval((re.sub(" +", " ", ((str(apriltags[i].center).replace("[", "")).replace("]", "")).strip())).replace(" ", ", "))
-    centerXY = list(centerXY)
-    centerXY[0] = centerXY[0] - 320
-    centerXY[1] = centerXY[1] - 240
-    print(" Tag:", apriltags[i].tag_id, "\n  X:", str(centerXY[0]), "\n  Y:", str(centerXY[1]))
-    if (apriltags[i].tag_id >= 1 and apriltags[i].tag_id <= 3) or apriltags[i].tag_id == 5:
-      img = cv2.putText(img, str(apriltags[i].tag_id), (int(centerXY[0]) + 300, int(centerXY[1]) + 260), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 6, 2)
-      img = cv2.putText(img, str(int(centerXY[0])) + ", " + str(int(centerXY[1])), (int(centerXY[0]) + 300, int(centerXY[1]) + 300), cv2.FONT_HERSHEY_SIMPLEX, 2/3, (0, 0, 255), 3, 2)
-    elif (apriltags[i].tag_id >= 6 and apriltags[i].tag_id <= 8) or apriltags[i].tag_id == 4:
-      img = cv2.putText(img, str(apriltags[i].tag_id), (int(centerXY[0]) + 300, int(centerXY[1]) + 260), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 6, 2)
-      img = cv2.putText(img, str(int(centerXY[0])) + ", " + str(int(centerXY[1])), (int(centerXY[0]) + 300, int(centerXY[1]) + 300), cv2.FONT_HERSHEY_SIMPLEX, 2/3, (255, 0, 0), 3, 2)
+  mask2 = cv2.inRange(img, (0, 150, 0), (360, 255, 255))
 
-    rioComms.send("apriltags", "Tag " + str(apriltags[i].tag_id) + " X", centerXY[0])
-    rioComms.send("apriltags", "Tag " + str(apriltags[i].tag_id) + " Y", centerXY[1])
+  mask = cv2.medianBlur(mask, 25)
+  mask2 = cv2.medianBlur(mask2, 25)
 
+  kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(15, 15))
+  mask = cv2.erode(mask, kernel)
+  mask2 = cv2.erode(mask2, kernel)
 
+  mask = cv2.dilate(mask, kernel)
+  mask2 = cv2.dilate(mask2, kernel)
 
-    print(apriltags[i].corners)
+  mask3 = cv2.bitwise_and(mask, mask2)
 
-    cornersXYValues = list(re.sub(" +", " ", ''.join((str(apriltags[i].corners).replace("[", "").replace("]", "").strip()).splitlines())).split(" "))
+  mask3 = cv2.medianBlur(mask3, 25)
 
-    tagWidth = abs(float(cornersXYValues[4]) - float(cornersXYValues[6]))
+  mask3 = cv2.erode(mask3, kernel)
+  mask3 = cv2.dilate(mask3, kernel)
 
-    rioComms.send("apriltags", "Tag" + str(apriltags[i].tag_id) + "Width", tagWidth)
+  cnts = cv2.findContours(mask3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+  cntsAreas = []
 
-  rioComms.send("apriltags", "Tags", len(apriltags))
+  for i in cnts[0]:
+    M = cv2.moments(i)
 
-  apriltagIDs = []
-  for i in range(len(apriltags)):
-    apriltagIDs.append(apriltags[i].tag_id)
+    cntsAreas.append(M["m00"])
 
-  for i in range(8):
-    if apriltagIDs.count(i+1) > 0:
-      tagVisible = 1
-    else:
-      tagVisible = 0
+  if len(cntsAreas) > 0:
+    maxValue = max(cntsAreas)
 
-    rioComms.send("apriltags", "Tag " + str(i + 1) + " Visible", tagVisible)
+    maxCntsIndex = cntsAreas.index(maxValue)
 
-  cv2.imshow("Camera", img)
+    if maxValue > 2000:
+      M = cv2.moments(cnts[0][maxCntsIndex])
+
+      cX = int((M["m10"] / M["m00"]))
+      cY = int((M["m01"] / M["m00"]))
+
+      initImg = cv2.circle(initImg, (math.floor(cX), math.floor(cY)), 20, (0, 0, 255), -1)
+      initImg = cv2.drawContours(initImg, cnts[0][maxCntsIndex], -1, (0, 255, 0), 3)
+
+      rioComms.send("cones", "Cone X", cX - 400)
+      rioComms.send("cones", "Cone Y", cY - 250)
+
+  cv2.imshow("initial image", initImg)
+  cv2.imshow("masked image", mask3)
 
   cv2.waitKey(5)
-
-
-
-
