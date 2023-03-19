d77a4d73d1882e1d0d304e2437d5903f47aeb3d4
Owen Beck <oebeck@gmail.com>
2023-03-19 12:15:56 -0400

    Prepared cube vision, will tune monday.

--- main.py
+++ main.py
@@ -22,48 +22,73 @@ while True:
 
   img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-  mask = cv2.inRange(img, (0, 0, 0), (25, 255, 255))
+  coneMask1 = cv2.inRange(img, (0, 0, 0), (25, 255, 255))
 
-  mask2 = cv2.inRange(img, (0, 150, 0), (360, 255, 255))
+  coneMask2 = cv2.inRange(img, (0, 150, 0), (360, 255, 255))
 
-  mask = cv2.medianBlur(mask, 25)
-  mask2 = cv2.medianBlur(mask2, 25)
+  coneMask1 = cv2.medianBlur(coneMask1, 25)
+  coneMask2 = cv2.medianBlur(coneMask2, 25)
 
   kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(15, 15))
-  mask = cv2.erode(mask, kernel)
-  mask2 = cv2.erode(mask2, kernel)
+  coneMask1 = cv2.erode(coneMask1, kernel)
+  coneMask2 = cv2.erode(coneMask2, kernel)
 
-  mask = cv2.dilate(mask, kernel)
-  mask2 = cv2.dilate(mask2, kernel)
+  coneMask1 = cv2.dilate(coneMask1, kernel)
+  coneMask2 = cv2.dilate(coneMask2, kernel)
 
-  mask3 = cv2.bitwise_and(mask, mask2)
+  coneMask3 = cv2.bitwise_and(coneMask1, coneMask2)
 
-  mask3 = cv2.medianBlur(mask3, 25)
+  coneMask3 = cv2.medianBlur(coneMask3, 25)
 
-  mask3 = cv2.erode(mask3, kernel)
-  mask3 = cv2.dilate(mask3, kernel)
+  coneMask3 = cv2.erode(coneMask3, kernel)
+  coneMask3 = cv2.dilate(coneMask3, kernel)
 
-  cnts = cv2.findContours(mask3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  coneCnts = cv2.findContours(coneMask3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  cntsAreas = []
+  coneCntsAreas = []
 
-  for i in cnts[0]:
+  cubeMask1 = cv2.inRange(img, (0, 0, 0), (360, 255, 255))
+
+  cubeMask2 = cv2.inRange(img, (0, 255, 0), (360, 255, 255))
+
+  cubeMask1 = cv2.medianBlur(cubeMask1, 25)
+  cubeMask2 = cv2.medianBlur(cubeMask2, 25)
+
+  kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(15, 15))
+  cubeMask1 = cv2.erode(cubeMask1, kernel)
+  cubeMask2 = cv2.erode(cubeMask2, kernel)
+
+  cubeMask1 = cv2.dilate(cubeMask1, kernel)
+  cubeMask2 = cv2.dilate(cubeMask2, kernel)
+
+  cubeMask3 = cv2.bitwise_and(cubeMask1, cubeMask2)
+
+  cubeMask3 = cv2.medianBlur(cubeMask3, 25)
+
+  cubeMask3 = cv2.erode(cubeMask3, kernel)
+  cubeMask3 = cv2.dilate(cubeMask3, kernel)
+
+  cubeCnts = cv2.findContours(cubeMask3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+  cubeCntsAreas = []
+
+  for i in coneCnts[0]:
     M = cv2.moments(i)
 
-    cntsAreas.append(M["m00"])
+    coneCntsAreas.append(M["m00"])
 
-  if len(cntsAreas) > 0:
-    maxValue = max(cntsAreas)
+  if len(coneCntsAreas) > 0:
+    maxValue = max(coneCntsAreas)
 
-    maxCntsIndex = cntsAreas.index(maxValue)
+    maxconeCntsIndex = coneCntsAreas.index(maxValue)
 
-    M = cv2.moments(cnts[0][maxCntsIndex])
+    M = cv2.moments(coneCnts[0][maxconeCntsIndex])
 
     cX = int((M["m10"] / M["m00"]))
     cY = int((M["m01"] / M["m00"]))
 
     #img = cv2.circle(img, (math.floor(cX), math.floor(cY)), 20, (0, 0, 255), -1)
-    #img = cv2.drawContours(img, cnts[0][maxCntsIndex], -1, (0, 255, 0), 3)
+    #img = cv2.drawContours(img, coneCnts[0][maxconeCntsIndex], -1, (0, 255, 0), 3)
 
     rioComms.send("cones", "Cone X", cX - 100)
     rioComms.send("cones", "Cone Y", cY - 62)
@@ -73,3 +98,30 @@ while True:
     rioComms.send("cones", "Cone X", 0)
     rioComms.send("cones", "Cone Y", 0)
     rioComms.send("cones", "Cone Visible", 0)
+
+  for i in cubeCnts[0]:
+    M = cv2.moments(i)
+
+    cubeCntsAreas.append(M["m00"])
+
+  if len(cubeCntsAreas) > 0:
+    maxValue = max(cubeCntsAreas)
+
+    maxcubeCntsIndex = cubeCntsAreas.index(maxValue)
+
+    M = cv2.moments(cubeCnts[0][maxcubeCntsIndex])
+
+    cX = int((M["m10"] / M["m00"]))
+    cY = int((M["m01"] / M["m00"]))
+
+    # img = cv2.circle(img, (math.floor(cX), math.floor(cY)), 20, (0, 0, 255), -1)
+    # img = cv2.drawContours(img, cubeCnts[0][maxcubeCntsIndex], -1, (0, 255, 0), 3)
+
+    rioComms.send("cubes", "Cube X", cX - 100)
+    rioComms.send("cubes", "Cube Y", cY - 62)
+    rioComms.send("cubes", "Cube Visible", 1)
+
+  else:
+    rioComms.send("cubes", "Cube X", 0)
+    rioComms.send("cubes", "Cube Y", 0)
+    rioComms.send("cubes", "Cube Visible", 0)
